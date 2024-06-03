4113738c34f3e89804528b42f216375a23f4ae0d
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-06-03 17:24:12 -0600

    fix problem

--- schema.sql
+++ schema.sql
@@ -11,12 +11,3 @@ CREATE TABLE IF NOT EXISTS post (
 
 
 
--- Configuracion de la base de datos
-
-CREATE TABLE IF NOT EXISTS post (
-	id INT AUTO_INCREMENT PRIMARY KEY, 
-	title VARCHAR(255) NOT NULL,
-	content TEXT NOT NULL,
-	banner TEXT NOT NULL,
-	locationU INT NOT NULL DEFAULT 0
-);
